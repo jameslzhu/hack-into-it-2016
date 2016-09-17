985c604d5d8c7e71970cfa73b1565f27d485515a
Tianrui Chen <richard.0.chen@gmail.com>
2016-09-17 03:19:59 -0700

    model updated

--- models/county.js
+++ models/county.js
@@ -2,7 +2,14 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
  
 var countySchema   = new Schema({
-    id: String
+    state: String,
+    county: String,
+    occupation: int,
+    sex: int,
+    age: int,
+    hire: int,
+    earn: double,
+    quarter: int
 });
  
 module.exports = mongoose.model('County', counSchema);
